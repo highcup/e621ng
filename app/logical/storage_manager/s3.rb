39999e527ada23783efe2c0eef1d915d3cbaf6bd
highcup <meeper@protonmail.com>
2020-12-02 01:15:18 -0600

    fixed s3.rb by added )

--- app/logical/storage_manager/s3.rb
+++ app/logical/storage_manager/s3.rb
@@ -49,7 +49,7 @@ class StorageManager::S3 < StorageManager
     IMAGE_TYPES.each do |type|
       data_bucket = client.Bucket.new(bucket)
       file = data_bucket.object(file_path(post, post.file_ext, type, false))
-      file.move_to(data_bucket.object(file_path(post, post.file_ext, type, true))
+      file.move_to(data_bucket.object(file_path(post, post.file_ext, type, true)))
     end
   end
 
@@ -57,7 +57,7 @@ class StorageManager::S3 < StorageManager
     IMAGE_TYPES.each do |type|
       data_bucket = client.Bucket.new(bucket)
       file = data_bucket.object(file_path(post, post.file_ext, type, true))
-      file.move_to(data_bucket.object(file_path(post, post.file_ext, type, false))
+      file.move_to(data_bucket.object(file_path(post, post.file_ext, type, false)))
     end
   end
 end
